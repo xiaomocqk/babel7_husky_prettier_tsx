6da86534e7b9ba69515d9115226f267cc916030f
xiaomocqk <xiaomocqk@163.com>
2021-05-01 11:10:05 +0800

    feat: update

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function App() {
+  const [value, setValue] = useState(0);
   return (
     <>
       <div className="abc">react app1</div>
