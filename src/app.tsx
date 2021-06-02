924c19fd4cf751f6daa1a9008d52b3c058e4e6cd
xiaomocqk <xiaomocqk@163.com>
2021-06-02 09:20:16 +0800

    feat: eslint

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,18 @@
-import React, { useState } from 'react';
+import corejs from "core-js";
+import React, { useEffect, useState } from "react";
+// import qs from 'qs'
 
 export default function App() {
   const [value, setValue] = useState(0);
+  useEffect(()=>{
+    setTimeout(() => {
+      setValue(1);
+    }, 1000);
+    console.log(corejs);
+  },[]);
   return (
     <>
-      <div className="abc">react app1</div>
+      <div className="abc">react app1{value}</div>
     </>
   );
 }
--- src/demo.js
+++ src/demo.js
@@ -1,21 +1,23 @@
-import React from 'react'
+import React from 'react';
 
-export const a = 1
-const arr = [1, 2, 3].map((n) => n + 1)[4, 5].forEach((n) => n)
+export const a = 1;
+const arr = [1, 2, 3].map((n) => n + 1)[(4, 5)].forEach((n) => n);
 
 class Foo {
-    constructor () {
-        // super()
-    }
+  constructor() {
+    // super()
+  }
 }
-const p = new Promise(resolve => {
-    setTimeout(() => {
-        resolve(1)
-    }, 1000);
-})
+const p = new Promise((resolve) => {
+  setTimeout(() => {
+    resolve(1);
+  }, 1000);
+});
 
 function App() {
-    return <>
-        <div>this is a paragraph</div>
+  return (
+    <>
+      <div>this is a paragraph</div>
     </>
+  );
 }
